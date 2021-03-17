80839df97db015c6e2c890345121bfd5bbd73887
therealyingtong <yingtong@z.cash>
2021-03-17 17:17:01 +0800

    Add Sinsemilla constants

--- src/constants.rs
+++ src/constants.rs
@@ -2,3 +2,23 @@
 
 /// $\ell^\mathsf{Orchard}_\mathsf{base}$
 pub(crate) const L_ORCHARD_BASE: usize = 255;
+
+// SWU hash-to-curve personalizations
+
+/// SWU hash-to-curve personalization for the spending key base point
+pub const SPENDING_KEY_GENERATOR_PERSONALIZATION: &str = "z.cash:Orchard";
+
+/// SWU hash-to-curve personalization for the group hash for key diversification
+pub const KEY_DIVERSIFICATION_PERSONALIZATION: &str = "z.cash:Orchard-gd";
+
+/// SWU hash-to-curve personalization for the value commitment generator
+pub const VALUE_COMMITMENT_PERSONALIZATION: &str = "z.cash:Orchard-cv";
+
+/// SWU hash-to-curve personalization for the note commitment generator
+pub const NOTE_COMMITMENT_PERSONALIZATION: &str = "z.cash:Orchard-NoteCommit";
+
+/// SWU hash-to-curve personalization for the IVK commitment generator
+pub const COMMIT_IVK_PERSONALIZATION: &str = "z.cash:Orchard-CommitIvk";
+
+/// SWU hash-to-curve personalization for the nullifier generator
+pub const NULLIFIER_K_PERSONALIZATION: &str = "z.cash:Orchard-Nullifier-K";
---
+++ src/primitives/sinsemilla/constants.rs
@@ -0,0 +1,93 @@
+//! Sinsemilla generators
+use halo2::pasta::pallas;
+
+/// Number of bits of each message piece in SinsemillaHashToPoint
+pub const K: usize = 10;
+
+/// The largest integer such that 2^c <= (r_P - 1) / 2, where r_P is the order
+/// of Pallas.
+pub const C: usize = 253;
+
+// Sinsemilla Q generators
+/// SWU hash-to-curve personalization for Sinsemilla Q generators
+pub const Q_PERSONALIZATION: &str = "z.cash:SinsemillaQ";
+
+/// Generator used in SinsemillaHashToPoint for note commitment
+pub const Q_NOTE_COMMITMENT_M_GENERATOR: (pallas::Base, pallas::Base, pallas::Base) = (
+    pallas::Base::from_raw([
+        0xcb32_150b_bd42_167f,
+        0x0d67_fa10_5c39_d544,
+        0xfc17_b491_75e9_5464,
+        0x372e_2f74_da30_0868,
+    ]),
+    pallas::Base::from_raw([
+        0x1156_abe0_9e25_60b6,
+        0xbce9_ca0e_3950_8035,
+        0x75e1_f360_d55a_fa25,
+        0x3b75_a3b1_6173_a8d7,
+    ]),
+    pallas::Base::from_raw([
+        0xdbc8_2388_468b_ac91,
+        0xc81b_f7f1_7f13_b769,
+        0x502e_6f16_dd62_7ff8,
+        0x2229_42f2_0a34_9ca1,
+    ]),
+);
+
+/// Generator used in SinsemillaHashToPoint for IVK commitment
+pub const Q_COMMIT_IVK_M_GENERATOR: (pallas::Base, pallas::Base, pallas::Base) = (
+    pallas::Base::from_raw([
+        0xf8c3_aa6a_5398_bda3,
+        0xc7b4_088e_c568_f566,
+        0x2bea_4757_25b9_3595,
+        0x0b5d_a9af_fb31_c540,
+    ]),
+    pallas::Base::from_raw([
+        0x8fec_5cdf_3c96_10ff,
+        0xec19_8983_4aa5_dc12,
+        0x52f7_002c_08f7_9661,
+        0x0333_96c7_3e0f_b9d8,
+    ]),
+    pallas::Base::from_raw([
+        0xfa62_2168_f5cd_0aac,
+        0xecf3_523b_842d_be49,
+        0x995a_92f4_a214_b6f7,
+        0x32ef_ef9b_fd54_f4a1,
+    ]),
+);
+
+// Sinsemilla S generators
+
+/// SWU hash-to-curve personalization for Sinsemilla S generators
+pub const S_PERSONALIZATION: &str = "z.cash:SinsemillaS";
+
+// TODO: inline the Sinsemilla S generators used in each round of the Sinsemilla hash
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::constants::{COMMIT_IVK_PERSONALIZATION, NOTE_COMMITMENT_PERSONALIZATION};
+    use halo2::{arithmetic::CurveExt, pasta::pallas};
+
+    #[test]
+    fn q_note_commitment_m() {
+        let hasher = pallas::Point::hash_to_curve(Q_PERSONALIZATION);
+        let point = hasher(&[NOTE_COMMITMENT_PERSONALIZATION.as_bytes(), b"-M"].concat());
+        let (x, y, z) = point.jacobian_coordinates();
+
+        assert_eq!(x, Q_NOTE_COMMITMENT_M_GENERATOR.0);
+        assert_eq!(y, Q_NOTE_COMMITMENT_M_GENERATOR.1);
+        assert_eq!(z, Q_NOTE_COMMITMENT_M_GENERATOR.2);
+    }
+
+    #[test]
+    fn q_commit_ivk_m() {
+        let hasher = pallas::Point::hash_to_curve(Q_PERSONALIZATION);
+        let point = hasher(&[COMMIT_IVK_PERSONALIZATION.as_bytes(), b"-M"].concat());
+        let (x, y, z) = point.jacobian_coordinates();
+
+        assert_eq!(x, Q_COMMIT_IVK_M_GENERATOR.0);
+        assert_eq!(y, Q_COMMIT_IVK_M_GENERATOR.1);
+        assert_eq!(z, Q_COMMIT_IVK_M_GENERATOR.2);
+    }
+}
